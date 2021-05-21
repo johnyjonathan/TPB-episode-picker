62f2119cfd6d0c9e54ba76965bed0d9381aa271f
johnyjonathan <80420454+johnyjonathan@users.noreply.github.com>
2021-05-21 22:56:49 +0200

    Update views.py

--- TBPrandomEpisode/views.py
+++ TBPrandomEpisode/views.py
@@ -3,7 +3,7 @@ from .functions import funcs
 from .functions import dictionaries
 
 tiles = True
-<<<<<<< HEAD
+
 
 
 def home(request):
@@ -18,17 +18,4 @@ def result(request):
        season, episode = funcs.random_episode(dictionaries.orginal_show)
        name_of_series, filter_seson = funcs.showName(season) 
     return render(request, 'tpbrandomepisode/random.html', {'season': filter_seson, 'name_of_series': name_of_series, 'episode': episode})
-=======
-
-def home(request):
-        
-    if request.method == 'GET':
-        return render(request, 'tpbrandomepisode/home.html')
-    elif request.POST['wybierz'] == 'all':
-        orginal_series = False
-        episode = funcs.Episode()
-        season, episode = episode.random_episode()
-        name_of_series, filter_seson = episode.showName(season)
-        return render(request, 'tpbrandomepisode/result.html',{'season': season, 'episode': episode, 'name_of_series': name_of_series, 'filter_seson': filter_seson})
->>>>>>> main
 
